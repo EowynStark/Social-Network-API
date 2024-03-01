03bcab217f1f975ba8abb3aee7a945bb7b09d39b
EowynStark <andwellm89@gmail.com>
2024-03-01 15:36:30 -0600

    initial framework of the reaction model

---
+++ models/reaction.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Reaction = mongoose.model('Reaction', {
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp)
+    }, _id:false
+});
+
+module.exports = Reaction;

9d1d62b9c16f93a40eed81d01166ddeb89640a84
EowynStark <andwellm89@gmail.com>
2024-03-01 17:21:17 -0600

    initial frame work of thoughtRoute file

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {Thought, User, Reaction} = require('../../models');
+
+// GET all thoughts
+
+// GET a single thought by its _id
+
+// POST to create a new thought
+
+// PUT to update a thought by its _id
+
+// DELETE to remove a thought by its _id
+
+// POST to create a reaction stored in a single thought's reactions array field
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+
+module.exports = router;

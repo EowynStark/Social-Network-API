35a77a3034455f140ea5fa12ae7c5e1659cfc027
EowynStark <andwellm89@gmail.com>
2024-03-01 15:54:57 -0600

    initial framework for thought model

---
+++ models/thought.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const Thought = mongoose.model('Thought', {
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    }, 
+    reactions: [{
+        reactionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId()
+        }, 
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        }
+}], timestamps: true
+});
+
+Thought.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+module.exports = Thought;

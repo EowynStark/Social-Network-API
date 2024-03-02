9287d5088128bbc12c7f6e9a35ae117efde91839
EowynStark <andwellm89@gmail.com>
2024-03-02 16:58:37 -0600

    initial framework of thoughtData file

---
+++ utils/thoughtData.js
@@ -0,0 +1,28 @@
+const thoughtData = [
+    {
+        thoughtText: "I love the new update!",
+        username: "emmyTest",
+        createdAt: new Date(),
+        reactions: []
+    },
+    {
+        thoughtText: "I can't wait for the new update!",
+        username: "mikeTest",
+        createdAt: new Date(),
+        reactions: []
+    },
+    {
+        thoughtText: "I'm so excited for the new update!",
+        username: "daveTest",
+        createdAt: new Date(),
+        reactions: []
+    },
+    {
+        thoughtText: "I'm ready to see the new update!",
+        username: "joeTest",
+        createdAt: new Date(),
+        reactions: []
+    },
+];
+
+module.exports = thoughtData;

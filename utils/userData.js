14dfcd92e8d526bfb8733a32c8f2620c1bdb0205
EowynStark <andwellm89@gmail.com>
2024-03-02 16:54:20 -0600

    initial framework for userData file

---
+++ utils/userData.js
@@ -0,0 +1,28 @@
+const userData = [
+    {
+        username: 'emmyTest',
+        email: 'emmy@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'mikeTest',
+        email: 'mikebikes@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'daveTest',
+        email: 'daveola@email.com',
+        thoughts: [],
+        friends: []
+    },
+    {
+        username: 'joeTest',
+        email: 'joeyboy@email.com',
+        thoughts: [],
+        friends: []
+    },
+];
+
+module.exports = userData;

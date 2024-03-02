8d7c68faa395ab382c02a075cc21a12354be5c54
EowynStark <andwellm89@gmail.com>
2024-03-02 17:01:40 -0600

    initial framework of reactionData file

---
+++ utils/reactionData.js
@@ -0,0 +1,28 @@
+const reactionData = [
+    {
+        reactionId: 1,
+        reactionBody: 'This is a great idea!',
+        username: 'mikeTest',
+        createdAt: new Date()
+    },
+    {
+        reactionId: 2,
+        reactionBody: 'I love this idea!',
+        username: 'daveTest',
+        createdAt: new Date()
+    },
+    {
+        reactionId: 3,
+        reactionBody: 'Best idea ever!',
+        username: 'joeTest',
+        createdAt: new Date()
+    },
+    {
+        reactionId: 4,
+        reactionBody: 'You guys are so smart!',
+        username: 'emmyTest',
+        createdAt: new Date()
+    },
+];
+
+module.exports = reactionData;

35c7f868be6f8c4a027d426dd9c3693360b1412b
EowynStark <andwellm89@gmail.com>
2024-03-05 16:15:19 -0600

    fixing reactionId value

--- utils/reactionData.js
+++ utils/reactionData.js
@@ -1,24 +1,26 @@
+const { Mongoose, mongo } = require("mongoose");
+
 const reactionData = [
     {
-        reactionId: 1,
+        reactionId: mongoose.Types.ObjectId(),
         reactionBody: 'This is a great idea!',
         username: 'mikeTest',
         createdAt: new Date()
     },
     {
-        reactionId: 2,
+        reactionId: mongoose.Types.ObjectId(),
         reactionBody: 'I love this idea!',
         username: 'daveTest',
         createdAt: new Date()
     },
     {
-        reactionId: 3,
+        reactionId: mongoose.Types.ObjectId(),
         reactionBody: 'Best idea ever!',
         username: 'joeTest',
         createdAt: new Date()
     },
     {
-        reactionId: 4,
+        reactionId: mongoose.Types.ObjectId(),
         reactionBody: 'You guys are so smart!',
         username: 'emmyTest',
         createdAt: new Date()

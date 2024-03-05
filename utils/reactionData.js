f86608e85a6d0c8ab4cddaa63c4d7f2d73ec2a53
EowynStark <andwellm89@gmail.com>
2024-03-05 16:16:39 -0600

    capitlization syntax error

--- utils/reactionData.js
+++ utils/reactionData.js
@@ -2,25 +2,25 @@ const { Mongoose, mongo } = require("mongoose");
 
 const reactionData = [
     {
-        reactionId: mongoose.Types.ObjectId(),
+        reactionId: Mongoose.Types.ObjectId(),
         reactionBody: 'This is a great idea!',
         username: 'mikeTest',
         createdAt: new Date()
     },
     {
-        reactionId: mongoose.Types.ObjectId(),
+        reactionId: Mongoose.Types.ObjectId(),
         reactionBody: 'I love this idea!',
         username: 'daveTest',
         createdAt: new Date()
     },
     {
-        reactionId: mongoose.Types.ObjectId(),
+        reactionId: Mongoose.Types.ObjectId(),
         reactionBody: 'Best idea ever!',
         username: 'joeTest',
         createdAt: new Date()
     },
     {
-        reactionId: mongoose.Types.ObjectId(),
+        reactionId: Mongoose.Types.ObjectId(),
         reactionBody: 'You guys are so smart!',
         username: 'emmyTest',
         createdAt: new Date()
